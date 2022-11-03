a62418eb4b6c46973ff67e268d44b20a845ee74d
Michael Asefon P <pelumiasefon@gmail.com>
2022-11-03 19:27:21 +0100

    [IMP] add success controller test

---
+++ tests/Unit/Http/Controllers/SuccessControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Http\Controllers;
+
+use App\Http\Controllers\SuccessController;
+use PHPUnit\Framework\TestCase;
+
+class SuccessControllerTest extends TestCase
+{
+
+    public function testDashboard()
+    {
+
+    }
+}
