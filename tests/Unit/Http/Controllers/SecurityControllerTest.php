cd572a3339c6fa23057279b196e10802bb231741
Michael Asefon P <pelumiasefon@gmail.com>
2022-11-03 19:45:37 +0100

    [IMP] add test login test

---
+++ tests/Unit/Http/Controllers/SecurityControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Http\Controllers;
+
+use App\Http\Controllers\SecurityController;
+use PHPUnit\Framework\TestCase;
+
+class SecurityControllerTest extends TestCase
+{
+
+    public function testLogin()
+    {
+
+    }
+}
